534ac9758cd1805f02774dffb2362030554832ff
Maowss <Maowss@gmail.com>
2019-11-26 18:57:00 -0300

    Teste

--- register.php
+++ register.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
     $sql = "UPDATE usuario SET email_usuario='$email', senha='$password', imagemUsuario='https://mindlinapp.herokuapp.com/imagens/' '$rm' '.jpg'  WHERE rm='$rm' AND rg='$rg' ";
 
     $response = mysqli_query($conn, $sql);
- 
+  
     if ($response) {
     	if (mysqli_affected_rows($conn) > 0) {
     		$result["success"] = "1";
